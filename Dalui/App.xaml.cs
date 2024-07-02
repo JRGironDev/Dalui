8613b8407d2ed922983aca424f7be4bac70fabff
José Quiñónez <joserobertoq@outlook.com>
2024-07-02 06:11:24 -0600

    Add Dashboard Page

---
+++ Dalui/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Dalui.Views;
+
+namespace Dalui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new DashboardView();
+	}
+}
---
+++ Dalui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Dalui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
